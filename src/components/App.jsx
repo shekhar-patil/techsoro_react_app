4b285e45dc68dd0adfb51d9ca77e8a045a6bb15c
shekhar-patil <patilshekhar900@gmail.com>
2020-03-25 19:09:27 +0530

    Add login page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+import Home from './Home';
+import Login from './Login';
+import Logout from './Logout';
+import ArticleList from './ArticleList';
+import ArticleInfo from './ArticleInfo';
+import ArticleAdd from './ArticleAdd';
+import ArticleEdit from './ArticleEdit';
+import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <Navigation />
+          <Main />
+        </div>
+      </Router>
+    );
+  }
 }
+
+
+const Navigation = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink></li>
+      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/articles">Articles</NavLink></li>
+      {
+        localStorage.getItem("jwt") ?
+          <li className="nav-item"><NavLink exact className="nav-link" to="/logout">Log Out</NavLink></li>
+        :
+          <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/login">Log In</NavLink></li>
+      }
+    </ul>
+  </nav>
+);
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/logout" component={Logout} />
+    <Route exact path="/articles" component={ArticleList} />
+    <Route exact path="/articles/new" component={ArticleAdd} />
+    <Route exact path="/articles/:id" component={ArticleInfo} />
+    <Route exact path="/articles/:id/edit" component={ArticleEdit} />
+  </Switch>
+);
 
 export default App;
